bcf70b1b13ba469c95eca54652efb489228a8f1f
Faizan Ahmad <ahmad13faizan@gmail.com>
2025-02-09 15:14:02 +0530

    Third commit

--- todoApp/src/index.js
+++ todoApp/src/index.js
@@ -1,10 +1,2 @@
 // src/index.js
 import './styles.scss';
-
-function component() {
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello, Webpack with ESLint and SCSS!';
-  return element;
-}
-
-document.body.appendChild(component());
